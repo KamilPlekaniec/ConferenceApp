17a000b42eb468b9225b722ea10571e39bbce5b2
Kamil <kp054467@student.ath.edu.pl>
2021-08-31 12:44:06 +0200

    added ConferenceUser model

---
+++ ConferenceApp/Models/ConfetenceUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceApp.Models
+{
+    public enum ConferenceType
+    {
+        Workshop,
+        Lecture,
+        Discussion,
+    }
+    public class ConfetenceUser
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ConferenceType ConferenceType { get; set; }
+        public string Photo { get; set; }
+    }
+}
