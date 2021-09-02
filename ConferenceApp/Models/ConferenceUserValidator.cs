700febca09a0ed3bd812d06bf5247e3ed161ac95
Kamil <kp054467@student.ath.edu.pl>
2021-09-02 15:14:49 +0200

    added ConferenceUserValidator from FluentValidation library

---
+++ ConferenceApp/Models/ConferenceUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceApp.Models
+{
+    public class ConferenceUserValidator : AbstractValidator<ConferenceUser>
+    {
+        public ConferenceUserValidator()
+        {
+            RuleFor(x => x.FirstName).NotNull().Length(3, 30);
+            RuleFor(x => x.LastName).NotNull().Length(3, 30);
+            RuleFor(x => x.Email).NotNull().EmailAddress();
+            RuleFor(x => x.ConferenceType).IsInEnum().NotNull();
+        }
+    }
+}
