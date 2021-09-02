6743a15eb1530261f5c4fb4892454ab5db6b087b
Kamil <kp054467@student.ath.edu.pl>
2021-09-02 14:49:28 +0200

    update Startup, registered Session service

--- ConferenceApp/Startup.cs
+++ ConferenceApp/Startup.cs
@@ -27,6 +27,14 @@ namespace ConferenceApp
             services.AddControllersWithViews();
             services.AddDbContext<UserContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("ConferenceUsersDB")));
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +53,8 @@ namespace ConferenceApp
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
