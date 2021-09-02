e8bed0f547a2be39c757367bbe35428bca70def7
Kamil <kp054467@student.ath.edu.pl>
2021-09-02 13:55:04 +0200

    added UserContext with EntityFrameworkCore

---
+++ ConferenceApp/Context/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using ConferenceApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceApp.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ConferenceUser> Users { get; set; }
+    }
+}
