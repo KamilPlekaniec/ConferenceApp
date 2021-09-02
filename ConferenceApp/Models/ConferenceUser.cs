c5a0170973510fccb0f451d28b8d84fe80f13228
Kamil <kp054467@student.ath.edu.pl>
2021-09-02 13:36:45 +0200

    update ConferenceUser model

--- ConferenceApp/Models/ConferenceUser.cs
+++ ConferenceApp/Models/ConferenceUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,17 +8,25 @@ namespace ConferenceApp.Models
 {
     public enum ConferenceType
     {
+        [Display(Name = "Warsztaty")]
         Workshop,
+        [Display(Name = "Wykłady")]
         Lecture,
+        [Display(Name = "Dyskusje")]
         Discussion,
     }
     public class ConferenceUser
     {
+        [Key]
         public int UserID { get; set; }
+        [Display(Name = "Imię")]
         public string FirstName { get; set; }
+        [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Typ konferencji")]
         public ConferenceType ConferenceType { get; set; }
+        [Display(Name = "Zdjęcie")]
         public string Photo { get; set; }
     }
 }
